767a7d3b129b91fb21c61d65757ad014800c3fb7
Chad_Barnes <Chad.A.Barnes@outlook.com>
2020-08-20 14:32:12 -0400

    Updated

---
+++ lib/post.rb
@@ -0,0 +1,29 @@
+class Post
+    attr_accessor :author, :title
+  
+    @@all = []
+  
+    def initialize(title)
+      @title = title
+      save
+    end
+  
+    def save
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+
+    def self.author_name
+        @author
+    end 
+   
+    def author_name
+        author.name if author
+    end 
+
+        
+
+  end
